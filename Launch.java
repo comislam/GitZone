28644ccec15842b47ba20ea2f71d94d56f2ecbb8
comislam <islamcse22@gmail.com>
2024-06-09 16:48:01 +0530

    Modified after cloning

--- Launch.java
+++ Launch.java
@@ -3,5 +3,12 @@ class Launch
 	public static void main(String [] args)
 	{
 		System.out.println("Exploring Git and GitHub");
+			
 	}
+		public static void fun()
+		{
+			int a = 10;
+			int b = 20;
+			int sum = a + b;
+		}
 }
